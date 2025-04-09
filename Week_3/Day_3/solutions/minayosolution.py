9f19fae060c670c86d3821bcd832d07b11e682a2
MinayoMelanie <melanieminayo00@gmail.com>
2025-04-09 19:03:50 +0300

    Mel Solution

--- Week_3/Day_3/solutions/minayosolution.py
+++ Week_3/Day_3/solutions/minayosolution.py
@@ -1,29 +1,33 @@
-class Node:
-    def __init__(self, value, left=None, right=None):
+class Node :
+    def __init__(self,value,left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
 
-class Solution:
-    def createCompleteBinaryTree(self, arr):
-        if not arr:  
+
+class Solution :
+    def createCompleteBinaryTree(self,arr):
+        if not arr :
             return None
         
-        # Create the root of the tree
         root = Node(arr[0])
         queue = [root]
-        index = 1 
+        index = 1
+
         while queue and index < len(arr):
-            current = queue.pop(0)  
-            # Insert the left child
+            current = queue.pop(0)
+
+
             if index < len(arr):
                 current.left = Node(arr[index])
-                queue.append(current.left) 
+                queue.append(current.left)
+                index += 1  # Move to next element after adding left child
             
             # Insert the right child
             if index < len(arr):
                 current.right = Node(arr[index])
-                queue.append(current.right)  # Add the right child to the queue
-                index += 1
+                queue.append(current.right)
+                index += 1  # Move to next element after adding right child
         
         return root
+        
